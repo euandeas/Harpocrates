b66367b4c208615dc0d80aeeea0e52d9a60d5ff6
Euan Deas <contact@euandeas.com>
2025-12-03 21:47:50 +0000

    better yay install

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,4 +1,13 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$HARPOCRATES_INSTALL/base.packages" | grep -v '^$')
 sudo pacman -S --noconfirm --needed "${packages[@]}"
-sudo pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si --noconfirm
+
+sudo pacman -Sy --needed --noconfirm base-devel
+cd /tmp
+rm -rf yay-bin
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd -
+rm -rf yay-bin
+cd ~
